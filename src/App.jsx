c4e56d5a9fc71035ff19d7f09f607d3f2b85d518
Mario Munteanu <mario.munteanu@cirvianum.cat>
2024-04-17 17:55:54 +0200

    Titol i radio afegit

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,68 @@
-import React, { useState } from "react"; // Importem React i la funció useState per a gestionar l'estat
-import Form from "./components/Form"; // Importem el component Form
+import React, { useState } from 'react';
+import Form from './components/Form';
 
 function App() {
-  // Definim l'estat inicial de l'aplicació
-  const [tipusEstudiant, setTipusEstudiant] = useState("no-graduat"); // Establim el tipus d'estudiant per defecte com a "no-graduat"
-  const [placesNoGraduats, setPlacesNoGraduats] = useState(60); // Establim el nombre de places per a no graduats
-  const [placesGraduats, setPlacesGraduats] = useState(40); // Establim el nombre de places per a graduats
+  const [tipusEstudiant, setTipusEstudiant] = useState('no-graduat');
+  const [placesNoGraduats, setPlacesNoGraduats] = useState(60); // Places per a No-Graduats
+  const [placesGraduats, setPlacesGraduats] = useState(40); // Places per a Graduats
 
-  // Funció que s'executa quan es canvia el valor del selector de tipus d'estudiant
   const handleChange = (e) => {
-    setTipusEstudiant(e.target.value); // Actualitzem el tipus d'estudiant amb el valor seleccionat
+    setTipusEstudiant(e.target.value);
   };
 
-  // Funció que s'executa quan es fa submit al formulari
   const handleFormSubmit = () => {
-    // Si l'estudiant és "no-graduat", disminuïm en 1 el nombre de places per a no graduats
-    // Si no, disminuïm en 1 el nombre de places per a graduats
-    tipusEstudiant === "no-graduat"
+    tipusEstudiant === 'no-graduat'
       ? setPlacesNoGraduats(placesNoGraduats - 1)
       : setPlacesGraduats(placesGraduats - 1);
   };
 
-  // Funció per a actualitzar el nombre de places disponibles
   const setPlacesDisponibles = (updatedPlaces) => {
-    // Si l'estudiant és "no-graduat", actualitzem el nombre de places per a no graduats
-    // Si no, actualitzem el nombre de places per a graduats
-    tipusEstudiant === "no-graduat"
+    tipusEstudiant === 'no-graduat'
       ? setPlacesNoGraduats(updatedPlaces)
       : setPlacesGraduats(updatedPlaces);
   };
 
-  // Calculem el nombre total de places disponibles
+  const handleRadioChange = (e) => {
+    setTipusEstudiant(e.target.value);
+  };
+
   const placesTotalsDisponibles = placesNoGraduats + placesGraduats;
 
-  // Renderitzem l'aplicació
   return (
-    <div className="flex flex-col items-center justify-center gap-5 h-screen">
-      <div className="tipusEstudiant">
-        <label className="mr-2">Selecciona Tipus d'Estudiant:</label>
-        <select
-          className="appDropDown border rounded-md py-1 px-2"
-          onChange={handleChange}
-          value={tipusEstudiant}
-        >
-          <option value="no-graduat">No Graduat</option>
-          <option value="graduat">Graduat</option>
-        </select>
+    <div className="flex h-screen flex-col items-center justify-center gap-5">
+      <h2 className="rounded-md bg-blue-200 px-4 py-2 text-blue-800">
+        Formulari d'inscripció d'estudiants
+      </h2>
+      <div className="tipusEstudiant flex items-center">
+        <input
+          type="radio"
+          id="no-graduat"
+          name="tipusEstudiant"
+          value="no-graduat"
+          checked={tipusEstudiant === 'no-graduat'}
+          onChange={handleRadioChange}
+        />
+        <label htmlFor="no-graduat" className="mr-1">
+          No Graduat
+        </label>
+        <input
+          type="radio"
+          id="graduat"
+          name="tipusEstudiant"
+          value="graduat"
+          checked={tipusEstudiant === 'graduat'}
+          onChange={handleRadioChange}
+        />
+        <label htmlFor="graduat" className="ml-1">
+          Graduat
+        </label>
       </div>
       <Form
         tipusEstudiantSelect={tipusEstudiant}
         onFormSubmit={handleFormSubmit}
         setPlacesDisponibles={setPlacesDisponibles}
         placesActuals={
-          tipusEstudiant === "no-graduat" ? placesNoGraduats : placesGraduats
+          tipusEstudiant === 'no-graduat' ? placesNoGraduats : placesGraduats
         }
       />
       <p>Places restants per a No-Graduats: {placesNoGraduats}</p>
